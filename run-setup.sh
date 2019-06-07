0c424ee102e4d914e5e3c22242d0378f36b1ca2e
jfosco <jfosco@localhost.localdomain>
2019-06-07 13:41:31 -0500

    postgres insatll works

--- run-setup.sh
+++ run-setup.sh
@@ -1,5 +1,6 @@
-cd ~/py/ansible/
-source ~/py/ansible/ansible/hacking/env-setup
-source ~/py/ansible/env/bin/activate
+cd ~/sys/ansible/
+export ANSIBLE_INVENTORY=~/sys/ansible/setup/ansible_hosts
+source ~/sys/ansible/ansible/hacking/env-setup
+source ~/sys/ansible/env/bin/activate
 cd ansible
 ansible-playbook ../setup/setup.yml -K
