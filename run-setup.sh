5ed3dc30fba44fb7ff76a7546a5de273ef0addd8
jfosco <jfosco@localhost.localdomain>
2019-09-16 08:58:35 -0500

    add airflow

--- run-setup.sh
+++ run-setup.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
+# To use this to run plays for a single tag or tags use
+# --tags "<comma seperated list of tags to run>"
 cd ~/sys/ansible/
 export ANSIBLE_INVENTORY=~/sys/ansible/setup/ansible_hosts
 source ~/sys/ansible/ansible/hacking/env-setup
 source ~/sys/ansible/env/bin/activate
 cd ansible
-ansible-playbook ../setup/setup.yml -K
+ansible-playbook ../setup/setup.yml -K $@
